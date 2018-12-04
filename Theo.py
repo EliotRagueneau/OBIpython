02cc3debee4a84753f84825c809865699aa71663
Theo Gauvrit <tgauvrit@emi.u-bordeaux1.fr>
2018-12-04 17:02:54 +0100

    azonaona

--- Theo.py
+++ Theo.py
@@ -1,11 +1,11 @@
 from typing import *
-from eliot_main import get_genetic_code, write_csv, read_csv
+from eliot_main import get_genetic_code, write_csv, read_csv, read_fasta
 
 
 def read_flat_file(filename):
     """Load a file in memory by returning a string
 
-            This function is written by Théo Gauvrit.
+            This function is written by Theo Gauvrit.
 
             Args:
                 filename: file to open
@@ -17,13 +17,13 @@ def read_flat_file(filename):
     fichier=open(filename,"r")
     txt = fichier.read()
     fichier.close()
-    return txt # ← 'LOCUS       NM_000518 ' ...
+    return txt #  'LOCUS       NM_000518 ' ...
 
 
 def reversed_complement(dna_seq):
     """Return the reversed complement of the given DNA sequence
 
-            This function is written by Théo GAUVRIT.
+            This function is written by Theo GAUVRIT.
 
             Args:
                 dna_seq: DNA sequence to be reversed.
@@ -41,7 +41,7 @@ def reversed_complement(dna_seq):
     return "".join(complement_seq)
 
 
-def find_orf(seq: str, threshold: int, code_table_id: int) -> [dict]:
+def find_orf(seq: str, threshold:int,code_table_id: int):
     """Give a list of all ORF in the sequence if they are grater than the threshold
 
             This function is written by .
@@ -57,5 +57,80 @@ def find_orf(seq: str, threshold: int, code_table_id: int) -> [dict]:
                     stop: stop position (in bp)
                     length: ORF length (in bp)
                     protein: translated protein sequence if available.
+                    frame:1,2,3 ou -1,-2,-3
         """
-    pass
+    transl_table, start_table = get_genetic_code(code_table_id)
+
+    ORF_lidi=[]
+    #ORF_lidi[e]={"start":0,"stop":0,"length":0,"protein":0,"frame":0}
+    start_lidi=[]
+    listestop=[]
+    stop_lidi=[]
+    orftrans=[]
+    rever=reversed_complement(seq)
+ 
+    
+    for z in transl_table.keys():
+        if transl_table[z]=='*':
+            listestop.append(z)
+            
+    for a in range(3):
+        for pos in range(a,len(seq)):
+            if seq[pos:pos+3] in start_table:
+                start_lidi.append({"pos":pos,"frame":a+1})
+            if rever[pos:pos+3] in start_table:
+                start_lidi.append({"pos":len(seq)-pos,"frame":-a-1})
+                
+            if seq[pos:pos+3] in listestop:
+                stop_lidi.append({"pos":pos,"frame":a+1})
+            if rever[pos:pos+3] in listestop:
+                stop_lidi.append({"pos":len(seq)-pos,"frame":-a-1})
+
+
+    for i in range(len(stop_lidi)):
+        for j in range(i,len(stop_lidi)):
+           if stop_lidi[j]["frame"]==stop_lidi[i]["frame"] and stop_lidi[j]["pos"]>stop_lidi[i]["pos"]:
+               
+               for k in range(len(start_lidi)):
+                  
+                   
+                  if stop_lidi[j]["frame"]>0:
+                      
+                      print(2135465464)
+                      break
+                  if start_lidi[k]["frame"]==stop_lidi[i]["frame"] and ((stop_lidi[j]["pos"])-(start_lidi[k]["pos"]))>threshold:
+                        print(3165464646464)
+                         
+                        for l in range(start_lidi[k]["pos"],stop_lidi[j]["pos"]):
+                              print(123123)
+                              print("ouiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+     #                           orftrans.append(transl_table[seq[l:l+3]])
+     #                       ORF_lidi.append({"start":k["pos"],"stop":j["pos"],"length":len(orftrans)*3,"protein":orftrans,"frame":k["frame"]})
+                           
+                   # else:
+                   #     if k["frame"]==i["frame"] and (k["pos"]-j["pos"])<(-threshold):
+                   #         for l in range(k["pos"]-3,j["pos"]):
+                   #             orftrans.append(transl_table[seq[l:l+3]])
+                   #         ORF_lidi.append({"start":k["pos"],"stop":j["pos"],"length":len(orftrans)*3,"protein":orftrans,"frame":k["frame"]})
+                           
+                   
+                  
+               
+               
+               
+               
+
+    
+    
+                
+
+                
+    return listestop,start_lidi,stop_lidi,ORF_lidi
+
+
+if __name__=="__main__":
+    
+   seqentry=read_fasta("influenza.fasta")
+   a,b,c,d=find_orf(seqentry,300,11)
+  
+   
